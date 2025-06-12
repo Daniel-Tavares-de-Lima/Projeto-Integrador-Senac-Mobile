a98fabd94ca08e829eac55472df86e9bec85979f
Daniel Tavares de Lima Marcelino <toys.tokio@gmail.com>
2025-06-12 06:21:26 -0300

    atualização

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { Image, View } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Image, View } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 // Telas
-import LoginScreen from './screens/loginScreen';
-import AuthLoadingScreen from './screens/AuthLoadingScreen';
-import HomeScreen from './screens/HomeScreen';
-import VictimsScreen from './screens/vitimaScreen';
-import CasesScreen from './screens/caseScreen';
-import EvidenciaScreen from './screens/EvidenciaScreen';
-import DashboardScreen from './screens/dashboardScreen';
-import ProfessionalScreen from './screens/ProfissionalScreen';
+import LoginScreen from "./screens/loginScreen";
+import AuthLoadingScreen from "./screens/AuthLoadingScreen";
+import HomeScreen from "./screens/HomeScreen";
+import VictimsScreen from "./screens/vitimaScreen";
+import CasesScreen from "./screens/caseScreen";
+import EvidenciaScreen from "./screens/EvidenciaScreen";
+import DashboardScreen from "./screens/dashboardScreen";
+import ProfessionalScreen from "./screens/ProfissionalScreen";
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -24,10 +28,16 @@ function CustomDrawerContent(props) {
   return (
     <View style={{ flex: 1 }}>
       {/* LOGO NO TOPO */}
-      <View style={{ alignItems: 'center', paddingVertical: 30, backgroundColor: '#3C68A2' }}>
+      <View
+        style={{
+          alignItems: "center",
+          paddingVertical: 30,
+          backgroundColor: "#3C68A2",
+        }}
+      >
         <Image
-          source={require('./assets/logo.png')} // Caminho da sua logo
-          style={{ width: 200, height: 120, resizeMode: 'contain' }}
+          source={require("./assets/logo.png")} // Caminho da sua logo
+          style={{ width: 200, height: 120, resizeMode: "contain" }}
         />
       </View>
 
@@ -46,14 +56,14 @@ function DrawerNavigator() {
       screenOptions={{
         headerShown: false,
         drawerStyle: {
-          backgroundColor: '#3C68A2',
+          backgroundColor: "#3C68A2",
           width: 250,
         },
-        drawerActiveTintColor: '#f5f5f5',
-        drawerInactiveTintColor: '#fff',
+        drawerActiveTintColor: "#f5f5f5",
+        drawerInactiveTintColor: "#fff",
         drawerLabelStyle: {
           fontSize: 18,
-          fontWeight: '500',
+          fontWeight: "500",
           marginLeft: -10,
         },
       }}
@@ -62,7 +72,7 @@ function DrawerNavigator() {
         name="Home"
         component={HomeScreen}
         options={{
-          title: 'Início',
+          title: "Início",
           drawerIcon: ({ color, size }) => (
             <Icon name="home-outline" color={color} size={size} />
           ),
@@ -72,7 +82,7 @@ function DrawerNavigator() {
         name="Victims"
         component={VictimsScreen}
         options={{
-          title: 'Vítimas',
+          title: "Vítimas",
           drawerIcon: ({ color, size }) => (
             <Icon name="account-heart-outline" color={color} size={size} />
           ),
@@ -82,7 +92,7 @@ function DrawerNavigator() {
         name="Cases"
         component={CasesScreen}
         options={{
-          title: 'Casos',
+          title: "Casos",
           drawerIcon: ({ color, size }) => (
             <Icon name="file-document-outline" color={color} size={size} />
           ),
@@ -93,7 +103,7 @@ function DrawerNavigator() {
         name="Profissionais"
         component={ProfessionalScreen}
         options={{
-          title: 'Profissionais',
+          title: "Profissionais",
           drawerIcon: ({ color, size }) => (
             <Icon name="file-document-outline" color={color} size={size} />
           ),
@@ -104,7 +114,7 @@ function DrawerNavigator() {
         name="Evidencia"
         component={EvidenciaScreen}
         options={{
-          title: 'Evidência',
+          title: "Evidência",
           drawerIcon: ({ color, size }) => (
             <Icon name="image-multiple-outline" color={color} size={size} />
           ),
@@ -115,9 +125,9 @@ function DrawerNavigator() {
         name="Dashboard"
         component={DashboardScreen}
         options={{
-          title: 'Dashboard',
+          title: "Dashboard",
           drawerIcon: ({ color, size }) => (
-            <Icon name="image-multiple-outline" color={color} size={size} />
+            <Icon name="chart-box-outline" color={color} size={size} />
           ),
         }}
       />
@@ -129,7 +139,10 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="AuthLoading" screenOptions={{ headerShown: false }}>
+        <Stack.Navigator
+          initialRouteName="AuthLoading"
+          screenOptions={{ headerShown: false }}
+        >
           <Stack.Screen name="AuthLoading" component={AuthLoadingScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Main" component={DrawerNavigator} />
